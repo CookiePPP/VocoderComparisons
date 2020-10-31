978b4beea3b43aee9b11addbf743c3db527e8031
CookiePPP <42448678+CookiePPP@users.noreply.github.com>
2020-10-31 22:31:57 +0000

    updated HiFi-GAN inference.py

--- repos/hifi-gan/inference.py
+++ repos/hifi-gan/inference.py
@@ -7,7 +7,8 @@ import json
 import torch
 from scipy.io.wavfile import write
 from env import AttrDict
-from meldataset import mel_spectrogram, MAX_WAV_VALUE, load_wav
+from nvSTFT import load_wav_to_torch
+from nvSTFT import STFT as STFT_Class
 from models import Generator
 
 h = None
@@ -22,10 +23,6 @@ def load_checkpoint(filepath, device):
     return checkpoint_dict
 
 
-def get_mel(x):
-    return mel_spectrogram(x, h.n_fft, h.num_mels, h.sampling_rate, h.hop_size, h.win_size, h.fmin, h.fmax)
-
-
 def scan_checkpoint(cp_dir, prefix):
     pattern = os.path.join(cp_dir, prefix + '*')
     cp_list = glob.glob(pattern)
@@ -34,30 +31,27 @@ def scan_checkpoint(cp_dir, prefix):
     return sorted(cp_list)[-1]
 
 
-def inference(a):
+def inference(a, STFT):
     generator = Generator(h).to(device)
-
+    
     state_dict_g = load_checkpoint(a.checkpoint_file, device)
     generator.load_state_dict(state_dict_g['generator'])
-
+    
     filelist = os.listdir(a.input_wavs_dir)
-
+    
     os.makedirs(a.output_dir, exist_ok=True)
-
+    
     generator.eval()
     generator.remove_weight_norm()
     with torch.no_grad():
         for i, filname in enumerate(filelist):
-            wav, sr = load_wav(os.path.join(a.input_wavs_dir, filname))
-            wav = wav / MAX_WAV_VALUE
-            wav = torch.FloatTensor(wav).to(device)
-            x = get_mel(wav.unsqueeze(0))
+            x = STFT(os.path.join(a.input_wavs_dir, filname)).to(device).unsqueeze(0)# [1, n_mel, dec_T]
             y_g_hat = generator(x)
             audio = y_g_hat.squeeze()
-            audio = audio * MAX_WAV_VALUE
+            audio = audio * (2**15)
             audio = audio.cpu().numpy().astype('int16')
-
-            output_file = os.path.join(a.output_dir, os.path.splitext(filname)[0] + '_generated.wav')
+            
+            output_file = os.path.join(a.output_dir, os.path.splitext(filname)[0] + '.wav')
             write(output_file, h.sampling_rate, audio)
             print(output_file)
 
@@ -67,7 +61,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_wavs_dir', default='test_files')
-    parser.add_argument('--output_dir', default='generated_files')
+    parser.add_argument('--output_dir',     default='test_files_generated')
     parser.add_argument('--checkpoint_file', required=True)
     a = parser.parse_args()
 
@@ -86,8 +80,11 @@ def main():
         device = torch.device('cuda')
     else:
         device = torch.device('cpu')
-
-    inference(a)
+    
+    global STFT
+    STFT = STFT_Class(h.sampling_rate, h.num_mels, h.n_fft, h.win_size, h.hop_size, h.fmin, h.fmax)
+    
+    inference(a, STFT)
 
 
 if __name__ == '__main__':
